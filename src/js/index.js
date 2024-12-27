11510c0ea7254a9b618100fd6112234499cf77ca
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-27 08:56:28 +0200

    documented mergeSort function

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+/**
+ * A function that uses merge sort to sort an array in ascending order.
+ * 
+ * @param {Array}  array - takes unsorted array as a parameter.
+ *
+ * @returns {Array} sortedArray - returns an array that is sorted in ascending order
+ */
+const mergeSort = (array) => {
+
+}
