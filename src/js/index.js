c3a7b96134049c6a21a23100d98ea64a2bc25b10
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-27 12:10:01 +0200

    mergeSort splits array into half

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,19 @@
 /**
  * A function that uses merge sort to sort an array in ascending order.
- * 
+ *
  * @param {Array}  array - takes unsorted array as a parameter.
  *
  * @returns {Array} sortedArray - returns an array that is sorted in ascending order
  */
-const mergeSort = (array) => {
 
-}
+let arrayUpToSeven = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(arrayUpToSeven.slice(0, arrayUpToSeven.length / 2));
+
+const mergeSort = (array) => {
+  if (array.length == 1) {
+    return;
+  } else {
+    array.slice(0, array.length);
+    mergeSort();
+  }
+};
